498ede7ca79f9a594e30ebfd5d2db431e8f95fca
Henry Schreiner <henryschreineriii@gmail.com>
2024-04-30 01:44:19 -0400

    feat: add schema and validate-pyproject support
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

---
+++ src/check_sdist/schema.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+import json
+from pathlib import Path
+from typing import Any
+
+from .resources import resources
+
+
+def get_schema(tool_name: str = "check-sdist") -> dict[str, Any]:
+    "Get the stored complete schema for check-sdist settings."
+    assert tool_name == "check-sdist", "Only check-sdist is supported."
+
+    with resources.joinpath("check-sdist.schema.json").open(encoding="utf-8") as f:
+        return json.load(f)  # type: ignore[no-any-return]
