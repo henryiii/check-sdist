349d5a0b4e0fb4094a830fe0faa8280578acf175
Henry Schreiner <HenrySchreinerIII@gmail.com>
2025-05-06 01:53:04 -0400

    feat: drop Python 3.8 (#107)

--- src/check_sdist/resources/__init__.py
+++ src/check_sdist/resources/__init__.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
-from .._compat.importlib import resources as _resources
+import importlib.resources
 
 __all__: list[str] = ["resources"]
 
 
-resources = _resources.files(__name__)
+resources = importlib.resources.files(__name__)
