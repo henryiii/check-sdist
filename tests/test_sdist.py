afc1e0d4bdf631a792ba0637aac1575ad703d08d
Henry Schreiner <HenrySchreinerIII@gmail.com>
2026-03-03 14:07:15 -0500

    feat: common generated files (#145)
    * feat: detect some generated files
    
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>
    
    * feat: include pdm
    
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>
    
    * docs: mention write_to
    
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>
    
    ---------
    
    Signed-off-by: Henry Schreiner <henryfs@princeton.edu>

---
+++ tests/test_sdist.py
@@ -0,0 +1,77 @@
+from __future__ import annotations
+
+from check_sdist._compat import tomllib
+from check_sdist.sdist import default_sdist_ignore
+
+
+def test_default_sdist_ignore_setuptools_scm_version_file() -> None:
+    pyproject_str = """
+    [tool.setuptools_scm]
+    version_file = "src/example/_version.py"
+    """
+    pyproject = tomllib.loads(pyproject_str)
+
+    ignore = set(default_sdist_ignore(pyproject))
+
+    assert "src/example/_version.py" in ignore
+
+
+def test_default_sdist_ignore_hatch_vcs_version_file() -> None:
+    pyproject_str = """
+    [tool.hatch.build.hooks.vcs]
+    version-file = "src/example/_version.py"
+    """
+    pyproject = tomllib.loads(pyproject_str)
+
+    ignore = set(default_sdist_ignore(pyproject))
+
+    assert "src/example/_version.py" in ignore
+
+
+def test_default_sdist_ignore_pdm_version_write_to() -> None:
+    pyproject_str = """
+    [tool.pdm.version]
+    write_to = "foo/_version.py"
+    """
+    pyproject = tomllib.loads(pyproject_str)
+
+    ignore = set(default_sdist_ignore(pyproject))
+
+    assert "foo/_version.py" in ignore
+
+
+def test_default_sdist_ignore_scikit_build_generate_paths() -> None:
+    pyproject_str = """
+    [[tool.scikit-build.generate]]
+    path = "src/example/_version.py"
+
+    [[tool.scikit-build.generate]]
+    template = "unused-no-path"
+
+    [[tool.scikit-build.generate]]
+    path = "python/pkg/version.py"
+    """
+    pyproject = tomllib.loads(pyproject_str)
+
+    ignore = set(default_sdist_ignore(pyproject))
+
+    assert "src/example/_version.py" in ignore
+    assert "python/pkg/version.py" in ignore
+
+
+def test_default_sdist_ignore_autogen_with_setuptools_defaults() -> None:
+    pyproject_str = """
+    [build-system]
+    build-backend = "setuptools.build_meta"
+
+    [tool.setuptools_scm]
+    version_file = "src/example/_version.py"
+    """
+    pyproject = tomllib.loads(pyproject_str)
+
+    ignore = set(default_sdist_ignore(pyproject))
+
+    assert "*.dist-info" in ignore
+    assert "*.egg-info" in ignore
+    assert "setup.cfg" in ignore
+    assert "src/example/_version.py" in ignore
